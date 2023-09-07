531abe75efd488dc7d0f370c81fe0ecc7a728cc7
Denloob <den@denloob.uk>
2023-09-07 10:48:08 +0300

    fix: apply all the rules at the same time
    Before, the rules were applied sequentially, which means that if rule 1 expands
    into char in rule 2, rule 2 will expand this char in the same call, which
    should not happen.

--- src/l_system.c
+++ src/l_system.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *l_system_apply_rule(const char *string, const Rule *rule)
+char *l_system_apply_rules(const char *string, const Rule *rules, int size)
 {
     size_t allocated = 1;
     char *res = xmalloc(1);
@@ -13,44 +13,29 @@ char *l_system_apply_rule(const char *string, const Rule *rule)
     char ch = 0;
     while ((ch = *string++))
     {
-        char *target = NULL;
-        char char_buf[2] = {0};
+        char *target = &ch;
+        size_t target_len = 1;
 
-        if (rule->from == ch)
+        for (const Rule *rule = rules; rule < rules + size; rule++)
         {
-            target = rule->to;
-        }
-        else
-        {
-            char_buf[0] = ch;
-            target = char_buf;
+            if (rule->from == ch)
+            {
+                target = rule->to;
+                target_len = strlen(rule->to);
+                break;
+            }
         }
 
-        while (allocated <= strlen(res) + strlen(target))
+        while (allocated <= strlen(res) + target_len)
         {
             allocated *= 2;
             res = xrealloc(res, allocated);
         }
 
-        strcat(res, target);
+        strncat(res, target, target_len);
     }
 
     res = xrealloc(res, strlen(res) + 1);
-
-    return res;
-}
-
-char *l_system_apply_rules(const char *string, const Rule *rules, int size)
-{
-    char *res = strdup(string);
-    char *temp = NULL;
-
-    for (const Rule *it = rules; it < rules + size; it++)
-    {
-        temp = l_system_apply_rule(res, it);
-        free(res);
-        res = temp;
-    }
 
     return res;
 }
--- src/l_system.h
+++ src/l_system.h
@@ -7,15 +7,6 @@ typedef struct Rule
 } Rule;
 
 /**
- * @brief Applies one rule to a string.
- *
- * @param string The string to apply the rule to.
- * @param rule The rule to apply.
- * @return String with the rule applied. (Managed by the caller).
- */
-char *l_system_apply_rule(const char *string, const Rule *rule);
-
-/**
  * @brief Applies all rules to a string.
  *
  * @param string The string to apply the rules to.
