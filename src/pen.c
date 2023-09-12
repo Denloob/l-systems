55be72ed41994b7c68897d392a9f4e83eccc04f7
Denloob <den@denloob.uk>
2023-09-12 09:58:43 +0300

    feat: add string-based pen command support

--- src/pen.c
+++ src/pen.c
@@ -1,6 +1,10 @@
 #include "SDL.h"
 #include "SDL2_gfxPrimitives.h"
 #include "pen.h"
+#include "utils.h"
+#include <string.h>
+
+#define ASCII_CHAR_AMOUNT ('~' + 1)
 
 void pen_move_forward(Pen *pen, float amount)
 {
@@ -15,4 +19,56 @@ void pen_move_forward(Pen *pen, float amount)
     }
 
     pen->pos = dest;
+}
+
+PenCommandRegistry pen_command_registry_create()
+{
+    PenCommandRegistry registry =
+        xmalloc(sizeof(*registry) * ASCII_CHAR_AMOUNT);
+    memset(registry, 0, sizeof(*registry) * ASCII_CHAR_AMOUNT);
+
+    return registry;
+}
+
+void pen_command_registry_destroy(PenCommandRegistry registry)
+{
+    free(registry);
+}
+
+bool pen_command_registry_add(PenCommandRegistry registry, char ch,
+                              PenCommand command)
+{
+    if (ch < 0 || ch >= ASCII_CHAR_AMOUNT || registry[(int)ch])
+        return false;
+
+    registry[(int)ch] = command;
+    return true;
+}
+
+bool pen_command_registry_remove(PenCommandRegistry registry, char ch)
+{
+    if (ch < 0 || ch >= ASCII_CHAR_AMOUNT)
+        return false;
+
+    registry[(int)ch] = NULL;
+    return true;
+}
+
+bool pen_command_registry_set(PenCommandRegistry registry, char ch,
+                              PenCommand command)
+{
+    if (ch < 0 || ch >= ASCII_CHAR_AMOUNT)
+        return false;
+
+    registry[(int)ch] = command;
+    return true;
+}
+
+void pen_commands_execute(Pen *pen, const PenCommandRegistry registry,
+                          const char *commands)
+{
+    for (const char *command = commands; *command; command++)
+    {
+        registry[(int)*command](pen);
+    }
 }
--- src/pen.h
+++ src/pen.h
@@ -13,9 +13,78 @@ typedef struct Pen
     bool down;
 } Pen;
 
+typedef void (*PenCommand)(Pen *pen);
+
+typedef PenCommand *PenCommandRegistry;
+
 /**
  * @brief Moves the pen forward. If it's down, will draw a line.
  *
  * @param amount The amount by which to move the pen forward.
  */
 void pen_move_forward(Pen *pen, float amount);
+
+/**
+ * @brief Creates a registry for pen commands.
+ *
+ * @return Pointer to the registry.
+ *
+ * @see pen_command_registry_destroy
+ */
+PenCommandRegistry pen_command_registry_create();
+
+/**
+ * @brief Destroys the given registry.
+ */
+void pen_command_registry_destroy(PenCommandRegistry registry);
+
+/**
+ * @brief Adds a pen command to the registry only if the char is not already
+ *          registered.
+ *
+ * @param ch The char to which the command will be registered.
+ * @param command The command to register.
+ *
+ * @return Whether the command was added.
+ *
+ * @see pen_command_registry_remove
+ * @see pen_command_registry_set
+ */
+bool pen_command_registry_add(PenCommandRegistry registry, char ch,
+                              PenCommand command);
+
+/**
+ * @brief Removes a pen command from the registry.
+ *
+ * @param ch The char to remove from the registry.
+ *
+ * @return Whether the command was removed.
+ *
+ * @see pen_command_registry_add
+ * @see pen_command_registry_set
+ */
+bool pen_command_registry_remove(PenCommandRegistry registry, char ch);
+
+/**
+ * @brief Sets a pen command in the registry.
+ *
+ * @param registry Pointer to the registry.
+ * @param ch The char to which the command will be registered.
+ * @param command The command to register.
+ * @return Whether the command was set.
+ *
+ * @see pen_command_registry_add
+ * @see pen_command_registry_remove
+ */
+bool pen_command_registry_set(PenCommandRegistry registry, char ch,
+                              PenCommand command);
+
+/**
+ * @brief Executes the pen commands using a given registry.
+ *
+ * @param pen The pen to use.
+ * @param registry The registry to use.
+ * @param commands The commands to execute.
+ */
+void pen_commands_execute(Pen *pen, const PenCommandRegistry registry,
+                          const char *commands);
